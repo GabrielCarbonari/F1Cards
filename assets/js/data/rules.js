326a4a766771ffe2264aa5e065c8118d4d46ece1
Edson Mendes <edson.luiz.mendes@hotmail.com>
2026-06-07 21:27:43 -0300

    feature: adicionar página game/rules.html

---
+++ assets/js/data/rules.js
@@ -0,0 +1,7 @@
+const rules = [
+  {rule: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus assumenda atque tenetur maxime incidunt ad eos, odit maiores totam molestiae earum sequi quaerat illum placeat modi iusto debitis quam voluptatibus.'},
+  {rule: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus assumenda atque tenetur maxime incidunt ad eos, odit maiores totam molestiae earum sequi quaerat illum placeat modi iusto debitis quam voluptatibus.'},
+  {rule: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus assumenda atque tenetur maxime incidunt ad eos, odit maiores totam molestiae earum sequi quaerat illum placeat modi iusto debitis quam voluptatibus.'},
+  {rule: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus assumenda atque tenetur maxime incidunt ad eos, odit maiores totam molestiae earum sequi quaerat illum placeat modi iusto debitis quam voluptatibus.'},
+  {rule: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus assumenda atque tenetur maxime incidunt ad eos, odit maiores totam molestiae earum sequi quaerat illum placeat modi iusto debitis quam voluptatibus.'},
+];
